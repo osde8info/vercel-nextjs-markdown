65115027a81f5d3ab22b5a6ada17830386a21187
clida <clida@DESKTOP-H4DBDL8>
2024-10-21 19:46:11 +0100

    commit2

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {ZZZ}
+        {XXX YYY ZZZ}
       </p>
       <div className="my-8">
         <BlogPosts />
